0900df0a11faf6c892fe9c1af15b78d05a679e47
Rolando Casigña <rolo4336@gmail.com>
2025-10-28 11:05:34 -0500

    REFAC: Actualiza la obtención del token JWT y mejora la gestión de errores en VigiflowCrawlerService. Cambia el path de Puppeteer y reorganiza las importaciones en vigiflow-integrator.module.ts. Se agrega archivo .env.example con configuraciones necesarias.

--- src/vigiflow-integrator/vigiflow-integrator.module.ts
+++ src/vigiflow-integrator/vigiflow-integrator.module.ts
@@ -1,24 +1,23 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { VigiflowIntegradorService } from './service/vigiflow-integrador.service';
+import { MeddraModule } from 'src/meddra/meddra.module';
+import { WhodrugsModule } from 'src/whodrugs/whodrugs.module';
+import { IntegratorModule } from '../integrator/integrator.module';
 import { VigiflowIntegradorController } from './controller/vigiflow-integrador.controller';
 import { VigiflowCrawlerService } from './service/vigiflow-crawler.service';
-import { HttpModule } from '@nestjs/axios';
-import { IntegratorModule } from '../integrator/integrator.module';
-import { WhodrugsModule } from 'src/whodrugs/whodrugs.module';
-import { MeddraModule } from 'src/meddra/meddra.module';
+import { VigiflowIntegradorService } from './service/vigiflow-integrador.service';
 
 @Module({
   imports: [
     HttpModule.register({
-      timeout: 100000,
+      timeout: 1000000000,
       maxRedirects: 5,
     }),
     IntegratorModule,
     WhodrugsModule,
-    MeddraModule
+    MeddraModule,
   ],
   providers: [VigiflowIntegradorService, VigiflowCrawlerService],
   controllers: [VigiflowIntegradorController],
 })
-
-export class VigiflowIntegratorModule { }
+export class VigiflowIntegratorModule {}
