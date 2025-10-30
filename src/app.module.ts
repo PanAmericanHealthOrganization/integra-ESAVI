014e8444f1ec3068a43013dcf967fa5be01e1b83
Rolando Casigña <rolo4336@gmail.com>
2025-10-29 21:37:50 -0500

    REFAC: Descomenta el módulo de vacunación en app.module.ts y mejora la lógica de filtrado en VacunometroService. Elimina la estrategia de autenticación por clave API.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,9 @@ import { Dhis2IntegratorModule } from './dhis2-integrator/dhis2-integrator.modul
 import { IntegratorModule } from './integrator/integrator.module';
 import { MeddraModule } from './meddra/meddra.module';
 import { HttpExceptionFilter } from './providers/http-exception.filter';
-import { HeaderApiKeyStrategy } from './strategy/header-api-key.strategy';
 //import { VacunacionIntegratorModule } from './vacunacion-integrator/vacunacion-integrator.module';
 import { VigiflowIntegratorModule } from './vigiflow-integrator/vigiflow-integrator.module';
 import { WhodrugsModule } from './whodrugs/whodrugs.module';
-import { VacunacionIntegratorModule } from './vacunacion-integrator/vacunacion-integrator.module';
 
 @Module({
   imports: [
@@ -20,13 +18,12 @@ import { VacunacionIntegratorModule } from './vacunacion-integrator/vacunacion-i
     }),
     IntegratorModule,
     Dhis2IntegratorModule,
-    VacunacionIntegratorModule, // Descomentar para habilitar el módulo de vacunación con base de datos oracle.
+    //VacunacionIntegratorModule, // Descomentar para habilitar el módulo de vacunación con base de datos oracle.
     VigiflowIntegratorModule,
     WhodrugsModule,
     MeddraModule,
   ],
   providers: [
-    HeaderApiKeyStrategy,
     {
       provide: APP_FILTER,
       useClass: HttpExceptionFilter,
