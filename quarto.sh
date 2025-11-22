4b1ccb1c3af3578de647e7f536572b6e91a730c0
Rolando Casigña <rolo4336@gmail.com>
2025-11-22 11:50:59 -0500

    Remove obsolete configuration files: pnpm-workspace.yaml, quarto.sh, and tsconfig.json

--- quarto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#Step 1: Download from webpage
-wget https://github.com/quarto-dev/quarto-cli/releases/download/v1.3.433/quarto-1.3.433-linux-amd64.deb
-#Step 2: Extract files
-dpkg -i quarto-1.3.433-linux-amd64.deb
