61603e0c70bcd4f67dca48d70fdfd92fcf7f2936
JP <20170202+tatcar55@users.noreply.github.com>
2025-12-16 00:36:10 -0500

    Update vite.config.js
    Cambio Config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 8080,
+        port: 8081,
+        
         open: true,
     },
     build: {
