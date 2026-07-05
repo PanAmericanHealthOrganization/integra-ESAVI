bd895cc5da4f9df2a075b3e8a60acad633e80218
Roberto Maldonado <robertomaldonadopa@gmail.com>
2026-07-05 16:28:36 -0500

    dhis 2

--- proyecto-02-integra-esavi/workspace/api-integra-esavi/src/dhis2-integrator/services/dhis2-options.service.ts
+++ proyecto-02-integra-esavi/workspace/api-integra-esavi/src/dhis2-integrator/services/dhis2-options.service.ts
@@ -14,15 +14,11 @@ export class Dhis2OptionsService {
   ) {}
 
   private getConfig() {
-    const username = this.configService.get<string>('DHIS2_USERNAME');
-    const passwd = this.configService.get<string>('DHIS2_PASSWD');
+    const token = this.configService.get<string>('DHIS2_API_TOKEN');
     return {
       maxBodyLength: Infinity,
       headers: {
-        Authorization: `Basic ${Buffer.from(
-          `${username}:${passwd}`,
-          'utf8',
-        ).toString('base64')}`,
+        Authorization: `ApiToken ${token}`,
       },
     };
   }
